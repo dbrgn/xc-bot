66443624485d8656f3722576d44c087060552990
Danilo Bargen <mail@dbrgn.ch>
2021-03-27 00:44:38 +0100

    Add database support

---
+++ migrations/20210327000400_create_flights.sql
@@ -0,0 +1,5 @@
+CREATE TABLE xcontest_flights (
+    url            STRING PRIMARY KEY NOT NULL,
+    title          STRING             NOT NULL,
+    pilot_username STRING             NOT NULL
+);
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::str::FromStr;
+
 use anyhow::Result;
+use sqlx::sqlite::{SqliteConnectOptions, SqliteJournalMode, SqlitePoolOptions};
 use tracing_log::LogTracer;
 use tracing_subscriber::{fmt::format::FmtSpan, FmtSubscriber};
 
@@ -9,14 +12,64 @@ async fn main() -> Result<()> {
     // Init logging
     LogTracer::init()?;
     let subscriber = FmtSubscriber::builder()
-        .with_env_filter("debug")
+        .with_env_filter("debug,sqlx::query=warn")
         .with_span_events(FmtSpan::CLOSE)
         .finish();
     tracing::subscriber::set_global_default(subscriber).expect("setting tracing default failed");
+    tracing::info!("Hello, pilots!");
 
-    tracing::info!("Hello, world!");
+    // Connect to database
+    let connect_options = SqliteConnectOptions::from_str("sqlite:data.db")?
+        .create_if_missing(true)
+        .journal_mode(SqliteJournalMode::Wal)
+        .foreign_keys(true);
+    let pool = SqlitePoolOptions::new()
+        .min_connections(2)
+        .max_connections(5)
+        .connect_with(connect_options)
+        .await?;
+
+    // Run migrations
+    sqlx::migrate!("./migrations").run(&pool).await?;
+
+    // Connect to XContest, fetch flights
     let xc = xcontest::XContest::new();
-    let channel = xc.fetch_flights().await?;
-    println!("{:#?}", channel);
+    let flights = xc.fetch_flights().await?;
+
+    // Process flights
+    let mut conn = pool.acquire().await?;
+    for flight in flights {
+        // Store flight in database.
+        let result = sqlx::query!(
+            r#"
+            INSERT INTO xcontest_flights (url, title, pilot_username)
+            VALUES (?, ?, ?)
+            "#,
+            flight.url,
+            flight.title,
+            flight.pilot_username,
+        )
+        .execute(&mut conn)
+        .await;
+
+        // If inserting fails with a unique constraint, that means that the
+        // flight was already processed before.
+        match result {
+            Err(sqlx::Error::Database(e)) if e.message() == "UNIQUE constraint failed: xcontest_flights.url" => {
+                tracing::debug!("Flight {} already processed, skipping", flight.url); 
+                continue;
+            }
+            Err(other) => {
+                // Uh oh...
+                tracing::error!("Error inserting flight {} into database: {}", flight.url, other);
+                continue;
+            },
+            Ok(_) => { /* Database entry did not yet exist, carry on with processing */ }
+        }
+
+        // Notify
+        tracing::info!("New flight: {}", flight.title);
+    }
+
     Ok(())
 }
