eddc31fd888819055bdc736f7d3281da0097acf4
Danilo Bargen <mail@dbrgn.ch>
2021-03-26 23:45:37 +0100

    Parse URLs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 use anyhow::Result;
+use tracing_log::LogTracer;
+use tracing_subscriber::{fmt::format::FmtSpan, FmtSubscriber};
 
 mod xcontest;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    println!("Hello, world!");
+    // Init logging
+    LogTracer::init()?;
+    let subscriber = FmtSubscriber::builder()
+        .with_env_filter("debug")
+        .with_span_events(FmtSpan::CLOSE)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("setting tracing default failed");
+
+    tracing::info!("Hello, world!");
     let xc = xcontest::XContest::new();
     let channel = xc.fetch_flights().await?;
     println!("{:#?}", channel);
--- src/xcontest.rs
+++ src/xcontest.rs
@@ -1,5 +1,7 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 
+use lazy_static::lazy_static;
+use regex::Regex;
 use reqwest::Client;
 
 const XCONTEST_URL: &str = "https://www.xcontest.org/rss/flights/?ccc";
@@ -12,6 +14,29 @@ pub struct XContest {
 pub struct Flight {
     pub title: String,
     pub url: String,
+    pub pilot_username: String,
+}
+
+impl Flight {
+    pub fn new(title: String, url: String) -> Result<Self> {
+        lazy_static! {
+            static ref RE: Regex = Regex::new(r"(?x)
+                http.*xcontest\.org.*
+                /detail:(?P<pilot>[^/]*)
+                /(?P<date>[^/]*)
+                /(?P<time>[0-2][0-9]:[0-6][0-9])
+            ").unwrap();
+        }
+        let caps = RE
+            .captures(&url)
+            .context(format!("Regex did not match XContest URL ({})", &url))?;
+        let pilot_username = caps.name("pilot").unwrap().as_str().to_string();
+        Ok(Self {
+            title,
+            url,
+            pilot_username,
+        })
+    }
 }
 
 impl XContest {
@@ -34,10 +59,32 @@ impl XContest {
             .into_items()
             .into_iter()
             .filter_map(|item: rss::Item| match (item.title, item.link) {
-                (Some(title), Some(link)) => Some(Flight { title, url: link }),
+                (Some(title), Some(link)) => match Flight::new(title, link) {
+                    Ok(flight) => Some(flight),
+                    Err(e) => {
+                        tracing::warn!("Could not parse flight URL: {}", e);
+                        None
+                    }
+                },
                 _ => None,
             })
             .collect::<Vec<Flight>>();
         Ok(flights)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_url() {
+        let title = "09.08.20 [21.98 km :: free_flight] Firstname Lastname".to_string();
+        let url = "https://www.xcontest.org/2020/switzerland/en/flights/detail:dbrgn/9.8.2020/10:45"
+            .to_string();
+        let flight = Flight::new(title.clone(), url.clone()).unwrap();
+        assert_eq!(flight.title, title);
+        assert_eq!(flight.url, url);
+        assert_eq!(flight.pilot_username, "dbrgn");
     }
 }
