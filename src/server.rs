7eb80b261ab8538e02c33c88776ee10ad1c7a897
Danilo Bargen <mail@dbrgn.ch>
2021-04-02 18:29:24 +0200

    Pass db pool to server, split up request handler

--- src/server.rs
+++ src/server.rs
@@ -2,6 +2,7 @@ use std::convert::Infallible;
 
 use bytes::Bytes;
 use hyper::{Body, Method, Request, Response, StatusCode};
+use sqlx::{Pool, Sqlite};
 
 fn http_200() -> Response<Body> {
     Response::builder()
@@ -25,10 +26,11 @@ fn http_500() -> Response<Body> {
 }
 
 /// Handle an incoming HTTP request.
-#[tracing::instrument(skip(req, api))]
+#[tracing::instrument(skip(req, api, pool))]
 pub async fn handle_http_request(
     req: Request<Body>,
     api: threema_gateway::E2eApi,
+    pool: Pool<Sqlite>,
 ) -> Result<Response<Body>, Infallible> {
     let path = req.uri().path();
     let method = req.method();
@@ -44,69 +46,10 @@ pub async fn handle_http_request(
                 }
             };
 
-            // Parse body
-            let msg = match api.decode_incoming_message(&body) {
-                Ok(data) => data,
-                Err(e) => {
-                    tracing::error!("Could not decode incoming Threema message: {}", e);
-                    return Ok(http_500());
-                }
-            };
-            let span = tracing::info_span!("incoming_message", from = &*msg.from, id = &*msg.message_id);
-            let _enter = span.enter();
-            tracing::trace!("Incoming message from {}", msg.from);
-            tracing::trace!("Raw message: {:?}", msg);
-
-            // Fetch sender public key
-            // TODO: Cache
-            let pubkey = match api.lookup_pubkey(&msg.from).await {
-                Ok(key) => key,
-                Err(e) => {
-                    tracing::error!("Could not fetch public key for {}: {}", &msg.from, e);
-                    return Ok(http_500());
-                }
-            };
-
-            // Decrypt
-            let data = match api.decrypt_incoming_message(&msg, &pubkey) {
-                Ok(key) => key,
-                Err(e) => {
-                    tracing::error!("Could not fetch public key for {}: {}", &msg.from, e);
-                    return Ok(http_500());
-                }
-            };
-            tracing::debug!("Decrypted data: {:?}", data);
-
-            // Handle depending on type
-            Ok(match data.get(0) {
-                Some(0x01) => {
-                    // Text message, UTF-8
-                    let text = match std::str::from_utf8(&data[1..]) {
-                        Ok(decoded) => decoded,
-                        Err(_) => {
-                            tracing::warn!("Received non-UTF8 bytes: {:?}, discarding", &data[1..]);
-                            return Ok(http_200());
-                        }
-                    };
-                    tracing::info!("Text: {:?}", text);
-                    http_200()
-                }
-                Some(0x80) => {
-                    // Delivery receipt, ignore
-                    tracing::info!("Ignoring delivery receipt");
-                    http_200()
-                }
-                Some(other) => {
-                    // Unsupported message type, ignore
-                    tracing::warn!("Ignoring unsupported message type: {}", other);
-                    http_200()
-                }
-                None => {
-                    // Empty data
-                    tracing::warn!("Incoming decrypted data is empty");
-                    http_500()
-                }
-            })
+            // Process message
+            let response = handle_threema_request(body, api, pool).await;
+            tracing::info!("Responding with HTTP {}", response.status());
+            Ok(response)
         }
 
         // Return 404 for all other requests
@@ -114,6 +57,76 @@ pub async fn handle_http_request(
             // Not found
             tracing::warn!("Unexpected HTTP {} request to {}", m, p);
             Ok(http_404())
+        }
+    }
+}
+
+pub async fn handle_threema_request(
+    bytes: Bytes,
+    api: threema_gateway::E2eApi,
+    _pool: Pool<Sqlite>,
+) -> Response<Body> {
+    // Parse body
+    let msg = match api.decode_incoming_message(bytes) {
+        Ok(data) => data,
+        Err(e) => {
+            tracing::error!("Could not decode incoming Threema message: {}", e);
+            return http_500();
+        }
+    };
+    let span = tracing::info_span!("incoming_message", from = &*msg.from, id = &*msg.message_id);
+    let _enter = span.enter();
+    tracing::trace!("Incoming message from {}", msg.from);
+    tracing::trace!("Raw message: {:?}", msg);
+
+    // Fetch sender public key
+    // TODO: Cache
+    let pubkey = match api.lookup_pubkey(&msg.from).await {
+        Ok(key) => key,
+        Err(e) => {
+            tracing::error!("Could not fetch public key for {}: {}", &msg.from, e);
+            return http_500();
+        }
+    };
+
+    // Decrypt
+    let data = match api.decrypt_incoming_message(&msg, &pubkey) {
+        Ok(key) => key,
+        Err(e) => {
+            tracing::error!("Could not fetch public key for {}: {}", &msg.from, e);
+            return http_500();
+        }
+    };
+    tracing::debug!("Decrypted data: {:?}", data);
+
+    // Handle depending on type
+    match data.get(0) {
+        Some(0x01) => {
+            // Text message, UTF-8
+            let text = match std::str::from_utf8(&data[1..]) {
+                Ok(decoded) => decoded,
+                Err(_) => {
+                    tracing::warn!("Received non-UTF8 bytes: {:?}, discarding", &data[1..]);
+                    return http_200();
+                }
+            };
+            tracing::info!("Text: {:?}", text);
+            http_200()
+        }
+        Some(0x80) => {
+            // Delivery receipt, ignore
+            tracing::info!("Ignoring delivery receipt");
+            http_200()
+        }
+        Some(other) => {
+            // Unsupported message type, ignore
+            tracing::warn!("Ignoring unsupported message type: {}", other);
+            http_200()
+        }
+        None => {
+            // Empty data
+            tracing::warn!("Incoming decrypted data is empty");
+            http_500()
         }
     }
 }
